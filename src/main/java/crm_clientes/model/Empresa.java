a1e58cfd0ce17600b76cd6a1c9af1e7b0db58a87
João Vitor Batista <joaovtb00@gmail.com>
2026-05-16 19:30:30 -0300

    mapeando entidades empresa e usuario com JPA

--- src/main/java/crm_clientes/model/Empresa.java
+++ src/main/java/crm_clientes/model/Empresa.java
@@ -1,4 +1,70 @@
 package crm_clientes.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "empresa")
 public class Empresa {
+
+    public Empresa(){
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID idEmpresa;
+
+    @Column(name = "nome_empresa", nullable = false, length = 150)
+    private String nomeEmpresa;
+
+    @Column(name = "cnpj_empresa", nullable = false, length = 18)
+    private String cnpjEmpresa;
+
+    @Column(name = "data_criacao", nullable = false)
+    private LocalDateTime dataCriacao;
+
+    @OneToMany(mappedBy = "empresa")
+    private List<Usuario> listUsuarios;
+
+    @PrePersist
+    public void prePersist() {
+        if (dataCriacao == null) {
+            this.dataCriacao = LocalDateTime.now();
+        }
+    }
+
+    public UUID getIdEmpresa() {
+        return idEmpresa;
+    }
+
+    public String getNomeEmpresa() {
+        return nomeEmpresa;
+    }
+
+    public String getCnpjEmpresa() {
+        return cnpjEmpresa;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setIdEmpresa(UUID idEmpresa) {
+        this.idEmpresa = idEmpresa;
+    }
+
+    public void setNomeEmpresa(String nomeEmpresa) {
+        this.nomeEmpresa = nomeEmpresa;
+    }
+
+    public void setCnpjEmpresa(String cnpjEmpresa) {
+        this.cnpjEmpresa = cnpjEmpresa;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
 }
--- src/main/java/crm_clientes/model/Usuario.java
+++ src/main/java/crm_clientes/model/Usuario.java
@@ -1,4 +1,41 @@
 package crm_clientes.model;
 
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "usuario")
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID idUsuario;
+
+    @Column(name = "nome_usuario", nullable = false, length = 150)
+    private String nomeUsuario;
+
+    @Column(name = "email_usuario", nullable = false, length = 150, unique = true)
+    private String emailUsuario;
+
+    @Column(name = "senha_hash", nullable = false, length = 255)
+    private String senhaHash;
+
+    @Column(name = "cargo_usuario", nullable = false)
+    private String cargoUsuario;
+
+    @Column(name = "ativo_usuario", nullable = false )
+    private boolean ativoUsuario;
+
+    @PrePersist
+    public void prePersist(){
+        if ((this.ativoUsuario == false)){
+            this.ativoUsuario = true;
+        }
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "empresa")
+    private Empresa empresaId;
+
 }
